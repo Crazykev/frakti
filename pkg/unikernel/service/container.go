7e519dcd9bd4620008d416ccdde2ca7670ac548a
Crazykev <crazykev@zju.edu.cn>
2017-08-25 00:11:08 +0800

    working
    Signed-off-by: Crazykev <crazykev@zju.edu.cn>

--- pkg/unikernel/service/container.go
+++ pkg/unikernel/service/container.go
@@ -18,12 +18,55 @@ package service
 
 import (
 	"fmt"
+	"github.com/docker/docker/pkg/stringid"
+	"k8s.io/frakti/pkg/unikernel/metadata"
 
 	kubeapi "k8s.io/kubernetes/pkg/kubelet/apis/cri/v1alpha1/runtime"
 )
 
 // CreateContainer creates a new container in specified PodSandbox
 func (u *UnikernelRuntime) CreateContainer(podSandboxID string, config *kubeapi.ContainerConfig, sandboxConfig *kubeapi.PodSandboxConfig) (string, error) {
+	var err error
+	name := config.GetMetadata().Name
+	// Check if there is any pod already created in pod
+	// We now only support one-container-per-pod
+	createdContainers := gerAllContainersInPod(podSandboxID)
+	for _, container := range createdContainers {
+		glog.Warningf("Unikernel/CreateContainer: container(%q) already exist in pod(%q), remove it", container.GetID(), podSandboxID)
+		if err := u.RemoveContainer(container.GetID()); err != nil {
+			glog.Errorf("Clean up legacy container(%q) failed: %v", container.GetID(), err)
+		}
+	}
+
+	sandbox, err := u.sandboxStore.Get(podSandboxID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get sandbox(%q) from store: %v", podSandboxID, err)
+	}
+
+	cid := generateID()
+	cName := makeContainerName(config.GetMetadata(), sandboxConfig.GetMetadata())
+	if err = u.containerNameIndex.Reserve(cName, cid); err != nil {
+		return nil, fmt.Errorf("reserve container name %q failed: %v", cName, err)
+	}
+	defer func() {
+		if err != nil {
+			u.containerNameIndex.ReleaseByName(cName)
+		}
+	}()
+
+	// Create internal container metadata
+	meta := metadata.ContainerMetadata{
+		ID:        cid,
+		Name:      cName,
+		SandboxID: podSandboxID,
+		Config:    config,
+	}
+
+	// TODO(Crazykev): Prepare container image
+
+	// Create container in VM, for now we actually create VM
+	u.vmTools.CreateContainer()
+
 	return "", fmt.Errorf("not implemented")
 }
 
@@ -51,4 +94,30 @@ func (u *UnikernelRuntime) ListContainers(filter *kubeapi.ContainerFilter) ([]*k
 // ContainerStatus returns the container status.
 func (u *UnikernelRuntime) ContainerStatus(containerID string) (*kubeapi.ContainerStatus, error) {
 	return nil, fmt.Errorf("not implemented")
+}
+
+func (u *UnikernelRuntime) getAllContainersInPod(podID string) []*metadata.ContainerMetadata {
+	containersInStore := u.containerStore.List()
+	var containers []*metadata.ContainerMetadata
+	for _, container := range containersInStore {
+		if container.SandboxID == podID {
+			containers = append(containers, container)
+		}
+	}
+	return containers
+}
+
+// generateID generates a random unique id.
+func generateID() string {
+	return stringid.GenerateNonCryptoID()
+}
+
+func makeContainerName(c *kubeapi.ContainerMetadata, s *kubeapi.PodSandboxMetadata) string {
+	return strings.Join([]string{
+		c.Name,
+		s.Name,
+		s.Namespace,
+		s.Uid,
+		fmt.Sprintf("%d", c.Attempt),
+	}, "_")
 }
